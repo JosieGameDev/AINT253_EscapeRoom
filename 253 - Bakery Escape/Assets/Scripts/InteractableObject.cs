d91294c32247c8e732e7269b8c946eeb85e5708f
JosieGameDev <43572951+JosieGameDev@users.noreply.github.com>
2019-11-15 12:52:09 +0000

    Started setting up hover and notes

---
+++ 253 - Bakery Escape/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InteractableObject : MonoBehaviour
+{
+    public string messageOnClick;
+    public GameObject feedbackUI;
+    private Animator objAnimator;
+    public Animation onClickAnim;
+    public Material glowMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        feedbackUI.SetActive(false);
+        objAnimator = this.gameObject.GetComponent<Animator>();
+        //glowMaterial.SetFloat
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(this.gameObject.name + " has been clicked");
+        Debug.Log(messageOnClick);
+        if (feedbackUI != null)
+        {
+            feedbackUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+
+        }
+
+        if(onClickAnim != null)
+        {
+            objAnimator.Play("");
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        Debug.Log("This object is interactable");
+        hoverLight.intensity = 2f;
+    }
+
+    private void OnMouseExit()
+    {
+        hoverLight.intensity = 0f;
+    }
+
+}
---
+++ 253 - Bakery Escape/Assets/Scripts/noteSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class noteSystem : MonoBehaviour
+{
+
+    public GameObject note;
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openNote()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void closeNote()
+    {
+        note.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
